9ed8b0ee18c375bf46f1ca376cf9415bf854b7dd
Beda <psyctoriya@gmail.com>
2025-09-04 14:34:43 +0300

    rabota7

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,18 +1,28 @@
-import {sortCollection, sortMap} from "../lib/sort.js";
+import {sortMap} from "../lib/sort.js";
 
 export function initSorting(columns) {
-    return (data, state, action) => {
+    return (query, state, action) => {
         let field = null;
         let order = null;
 
         if (action && action.name === 'sort') {
-            // @todo: #3.1 — запомнить выбранный режим сортировки
-
-            // @todo: #3.2 — сбросить сортировки остальных колонок
+            action.dataset.value = sortMap[action.dataset.value];
+            field = action.dataset.field;
+            order = action.dataset.value;
+            
+            columns.forEach(col => {
+                if (col !== action) col.dataset.value = 'none';
+            });
         } else {
-            // @todo: #3.3 — получить выбранный режим сортировки
+            columns.forEach(col => {
+                if (col.dataset.value !== 'none') {
+                    field = col.dataset.field;
+                    order = col.dataset.value;
+                }
+            });
         }
 
-        return sortCollection(data, field, order);
-    }
+        const sort = (field && order !== 'none') ? `${field}:${order}` : null;
+        return sort ? Object.assign({}, query, { sort }) : query;
+    };
 }
